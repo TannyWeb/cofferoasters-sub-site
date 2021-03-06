0ceb00c5e2f861f9140c6db2db68526c824d7c1b
tanvir <tanvir.ali@iprospect.com>
2021-03-06 11:18:25 +0000

    first commit

---
+++ src/js/functions/menu.js
@@ -0,0 +1,19 @@
+// Dummy code ------- can remove
+
+export const greeting = () => {
+	console.log('hello you ');
+};
+
+export const toggleMenu = () => {
+	const menuToggle = document.querySelector('.header__toggle');
+	const overlay = document.querySelector('.overlay');
+	menuToggle.addEventListener('click', () => {
+		if (overlay.classList.contains('hide')) {
+			overlay.classList.remove('hide');
+			overlay.classList.add('show');
+		} else {
+			overlay.classList.remove('show');
+			overlay.classList.add('hide');
+		}
+	});
+};
---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+// Dummy imports
+import '../scss/main.scss';
+
+import { toggleMenu } from './functions/menu';
+
+console.log('index loaded');
+// menu();
+toggleMenu();
